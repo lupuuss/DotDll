bec173248fe70ca6f374ce3b863f38c17ab9bcf0
Damian Baczyński <224250@edu.p.lodz.pl>
2020-12-01 17:37:23 +0100

    Code cleanup

--- DotDll.Tests.Presentation/ViewModel/MenuViewModelTest.cs
+++ DotDll.Tests.Presentation/ViewModel/MenuViewModelTest.cs
@@ -11,16 +11,6 @@ namespace DotDll.Tests.Presentation.ViewModel
     [TestFixture]
     public class MenuViewModelTest
     {
-        private Mock<IMetaDataService> _serviceMock;
-
-        private Mock<INavigator> _navigatorMock;
-
-        private Mock<IUserInputService> _userInputService;
-
-        private readonly string _pickedPath = "picked/path";
-        
-        private MenuViewModel _viewModel;
-
         [SetUp]
         public void SetUp()
         {
@@ -29,11 +19,21 @@ namespace DotDll.Tests.Presentation.ViewModel
             _userInputService = new Mock<IUserInputService>();
 
             _userInputService
-                .Setup((service) => service.PickFilePath())
-                .Returns(Task.FromResult(_pickedPath));
-            
+                .Setup(service => service.PickFilePath())
+                .Returns(Task.FromResult(ExpectedPickedPath));
+
             InitViewModel();
         }
+
+        private Mock<IMetaDataService> _serviceMock;
+
+        private Mock<INavigator> _navigatorMock;
+
+        private Mock<IUserInputService> _userInputService;
+
+        private const string ExpectedPickedPath = "picked/path";
+
+        private MenuViewModel _viewModel;
 
         private void InitViewModel()
         {
@@ -47,74 +47,72 @@ namespace DotDll.Tests.Presentation.ViewModel
         [Test]
         public void PickFileCommand_InvalidPath_SetsPathErrorMessageShown()
         {
-
             // setup
-            
+
             _serviceMock
-                .Setup((service) => service.IsValidFileSourcePath(It.IsAny<string>()))
+                .Setup(service => service.IsValidFileSourcePath(It.IsAny<string>()))
                 .Returns(false);
-            
+
             InitViewModel();
 
             _viewModel.PathErrorMessageShown = false;
 
             // run
-            
+
             _viewModel.PickFileCommand.Execute(null);
-            
+
             // verification
-            
+
             _serviceMock.Verify(
-                (service) => service.IsValidFileSourcePath(It.IsAny<string>()),
+                service => service.IsValidFileSourcePath(It.IsAny<string>()),
                 Times.Once
-                );
-            
+            );
+
             Assert.True(_viewModel.PathErrorMessageShown);
         }
-        
+
         [Test]
         public void PickedFilePath_ValidPath_ResetsPathErrorMessageAndNavigatesToMetaDataView()
         {
-            
             // setup
-            
+
             var pickedSource = new Mock<Source>().Object;
             _serviceMock
-                .Setup((service) => service.CreateFileSource(It.IsAny<string>()))
+                .Setup(service => service.CreateFileSource(It.IsAny<string>()))
                 .Returns(pickedSource);
-            
+
             _serviceMock
-                .Setup((service) => service.IsValidFileSourcePath(It.IsAny<string>()))
+                .Setup(service => service.IsValidFileSourcePath(It.IsAny<string>()))
                 .Returns(true);
-            
+
             InitViewModel();
-            
+
             _viewModel.PathErrorMessageShown = true;
 
             // run
-            
+
             _viewModel.PickFileCommand.Execute(null);
-            
+
             // verification
-            
+
             _serviceMock.Verify(
-                (service) => service.IsValidFileSourcePath(It.Is<string>(p => p == _pickedPath)),
+                service => service.IsValidFileSourcePath(It.Is<string>(p => p == ExpectedPickedPath)),
                 Times.Once
             );
-            
+
             _serviceMock.Verify(
-                (service) => service.CreateFileSource(It.Is<string>(p => p == _pickedPath)),
+                service => service.CreateFileSource(It.Is<string>(p => p == ExpectedPickedPath)),
                 Times.Once
-                );
+            );
 
             _navigatorMock.Verify(
-                (navigator) => navigator.NavigateTo(
-                    TargetView.MetaData, 
+                navigator => navigator.NavigateTo(
+                    TargetView.MetaData,
                     It.Is<Source>(s => s == pickedSource)
-                    ),
+                ),
                 Times.Once
-                );
-            
+            );
+
             Assert.AreEqual(pickedSource, _viewModel.Source);
             Assert.False(_viewModel.PathErrorMessageShown);
         }

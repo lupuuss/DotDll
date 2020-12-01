16c8ed16aea1e7cc64a0043f4586ffbca2547a2f
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-12-01 15:00:46 +0100

    Adds BaseViewModelTest.cs

---
+++ DotDll.Tests.Presentation/ViewModel/BaseViewModelTest.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using DotDll.Presentation.ViewModel.Common;
+using NUnit.Framework;
+
+namespace DotDll.Tests.Presentation.ViewModel
+{
+    class TestBaseViewModel : BaseViewModel
+    {
+        private string _testField;
+        
+        public string TestField
+        {
+            get => _testField;
+            set
+            {
+                if (_testField == value) return;
+
+                _testField = value;
+                OnPropertyChangedAuto();
+            }
+        }
+    }
+
+    [TestFixture]
+    public class BaseViewModelTest
+    {
+        private TestBaseViewModel _baseViewModel;
+        
+        [SetUp]
+        public void SetUp() {
+            _baseViewModel = new TestBaseViewModel();
+        }
+
+        [Test]
+        public void BaseViewModel_Always_ProperlyNotifiesAboutFieldChanges()
+        {
+            var handlerCalledProperly = false;
+            
+            _baseViewModel.PropertyChanged += delegate(object sender, PropertyChangedEventArgs args)
+            {
+                var actual = args.PropertyName;
+                
+                Assert.AreEqual("TestField", actual);
+                Assert.IsInstanceOf<TestBaseViewModel>(sender);
+                Assert.AreEqual("Test value", ((TestBaseViewModel) sender).TestField);
+                
+                handlerCalledProperly = true;
+            };
+
+            _baseViewModel.TestField = "Test value";
+            
+            Assert.True(handlerCalledProperly);
+        }
+    }
+}

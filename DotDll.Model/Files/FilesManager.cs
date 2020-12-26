7ff0b1d8b1d0503b7ebed4d12a2539ce696f8708
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-12-26 21:07:01 +0100

    Adds IFilesManager.cs with first implementation

---
+++ DotDll.Model/Files/FilesManager.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace DotDll.Model.Files
+{
+    public class FilesManager : IFilesManager
+    {
+        public bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public string GetExtension(string path)
+        {
+            return Path.GetExtension(path);
+        }
+    }
+}
---
+++ DotDll.Model/Files/IFilesManager.cs
@@ -0,0 +1,9 @@
+﻿namespace DotDll.Model.Files
+{
+    public interface IFilesManager
+    {
+        bool FileExists(string path);
+
+        string GetExtension(string path);
+    }
+}

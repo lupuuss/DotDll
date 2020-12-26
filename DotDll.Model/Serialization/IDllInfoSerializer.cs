170dcbcfbd3f1569159db249b24fc92e37d519c4
Filip.Kowalski195 <Filip.Kowalski195@gmail.com>
2020-12-26 20:55:56 +0100

    Adds IDllInfoSerializer.cs

---
+++ DotDll.Model/Serialization/IDllInfoSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DotDll.Model.Analysis.Data;
+
+namespace DotDll.Model.Serialization
+{
+    public interface IDllInfoSerializer
+    {
+        List<string> GetAllIds();
+
+        DllInfo Deserialize(string id);
+
+        void Serialize(DllInfo dllInfo);
+    }
+}

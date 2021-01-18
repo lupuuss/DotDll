c4bd030a4ad5c3b4be36263646fa2ab39570a22b
Damian Baczyński <224250@edu.p.lodz.pl>
2021-01-18 23:28:28 +0100

    Removes unused field in ConstructorNode.cs

--- DotDll.Presentation.Model/ConstructorNode.cs
+++ DotDll.Presentation.Model/ConstructorNode.cs
@@ -4,11 +4,8 @@ namespace DotDll.Presentation.Model
 {
     public class ConstructorNode : MemberNode<Constructor>
     {
-        private readonly Constructor _constructor;
-
         public ConstructorNode(Constructor constructor) : base(constructor)
         {
-            _constructor = constructor;
             Name = $"{GetAttributesString(constructor)}{GetAccessString(constructor.AccessLevel)} " + 
                    $"{constructor.Name}{MapParameters(constructor.Parameters)}";
         }
